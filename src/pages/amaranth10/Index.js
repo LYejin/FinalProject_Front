40d2ae14cf5ba590e94f2f1fe7a89c81543c1c53
LYejin <vgg789@naver.com>
2023-08-14 20:12:35 +0900

    Feat : amaranth10 Page index 생성

---
+++ src/pages/amaranth10/Index.js
@@ -0,0 +1,5 @@
+import CompanyPage from "./CompanyPage";
+import EmployeePage from "./EmployeePage";
+import WorkplacePage from "./WorkplacePage";
+
+export { CompanyPage, EmployeePage, WorkplacePage };
