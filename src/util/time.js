f7d6493c006f8ec0a9acb0e7135f4ac50b41615a
LYejin <vgg789@naver.com>
2023-08-16 11:56:02 +0900

    Feat : 사원 디테일 정보 보기 기능 구현

--- src/util/time.js
+++ src/util/time.js
@@ -1,7 +1,15 @@
-export const getCreateAtTime = data => {
+export const getCreateAtTime = (data) => {
   return (
     String(data.createAt).slice(0, 10) +
     " " +
     String(data.createAt).slice(11, 16)
   );
+};
+
+export const getNowTime = () => {
+  let now = new Date();
+  let todayYear = now.getFullYear();
+  let todayMonth = now.getMonth() + 1;
+  let todayDate = now.getDate();
+  return todayYear + "-" + todayMonth + "-" + todayDate;
 };
