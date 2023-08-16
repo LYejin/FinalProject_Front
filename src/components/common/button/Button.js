bcaa36f583f0c13fad4de6205e76b11c36253780
LYejin <vgg789@naver.com>
2023-08-16 11:51:04 +0900

    Feat : button component 생성

--- src/components/common/button/Button.js
+++ src/components/common/button/Button.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ButtonMUI from "@mui/material/Button";
 
-const Button = ({ data }) => {
+const Button = ({ data, width, height }) => {
   return (
     <ButtonMUI
       variant="contained"
       size="small"
       sx={{
         backgroundColor: "#3D4349",
+        width: width,
+        height: height,
         "&:hover": {
           backgroundColor: "#979797",
         },
