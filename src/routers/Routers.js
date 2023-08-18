50ed4a9bff630e4b6711fb0cfbcb07b542a72bbd
songhasuk <shs1995@naver.com>
2023-08-18 20:22:08 +0900

    Feat : 회사 조회, 상세정보 기능 구현

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -4,7 +4,6 @@ import Todo from "../pages/Todo/Todo";
 import RegisterForm from "../pages/login/RegisterForm";
 import LoginHookForm from "../pages/login/LoginHookForm";
 import { URL } from "../constants/router";
-import TestGrid from "./../pages/TestGrid";
 import {
   CompanyPage,
   EmployeePage,
@@ -17,7 +16,6 @@ const Routers = () => {
       <Route path={URL.home} element={<LoginHookForm />} />
       <Route path={URL.main} element={<Todo />} />
       <Route path={URL.register} element={<RegisterForm />} />
-      <Route path={URL.test} element={<TestGrid />} />
       <Route path={URL.company} element={<CompanyPage />} />
       <Route path={URL.employee} element={<EmployeePage />} />
       <Route path={URL.workplace} element={<WorkplacePage />} />
