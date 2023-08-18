50a178e1580ff0945bf65f7555e55c08913c30dd
songhasuk <shs1995@naver.com>
2023-08-18 20:21:28 +0900

    Feat : 회사 컴포넌트 구현

--- src/components/layout/amaranth/ListBoxItem.js
+++ src/components/layout/amaranth/ListBoxItem.js
@@ -1,14 +1,22 @@
 import React from "react";
 
-const ListBoxItem = ({ leftTop, rightTop, leftBottom }) => {
+const ListBoxItem = ({ leftTop, rightTop, leftBottom, rightBotton, clickEmp}) => {
+
+  const handleItemClick = () => {
+    clickEmp(leftTop);
+  };
+  
+  
+
   return (
-    <div className="listBoxItem">
+    <div className="listBoxItem" onClick={handleItemClick}>
       <div className="flexWrapper">
         <span className="leftContent">{leftTop}</span>
         <span className="rightContent">{rightTop}</span>
       </div>
       <div className="flexWrapper">
         <span className="code">{leftBottom}</span>
+        <span className="rightContent">{rightBotton}</span>
       </div>
     </div>
   );
--- src/components/layout/amaranth/SelectListWrapper.js
+++ src/components/layout/amaranth/SelectListWrapper.js
@@ -1,13 +1,98 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import ListBoxItem from "./ListBoxItem";
+import axios from "../../../../node_modules/axios/index";
+import CompanyModel from "../../../model/CompanyModel";
 
-const SelectListWrapper = ({ width, title, dataCount, data }) => {
+
+const SelectListWrapper = ({ width, title, dataCount, data, formData,formDataSet }) => {
   const selectListWrapper = {
     position: "relative",
     width: width,
     height: "100%",
     border: "1px solid #ebebeb",
+  }; 
+  const [listData, listDataSet] = useState()
+  const asyncRequest = async (url, methodType, data, headers) => {
+    const cookies= document.cookie
+    const token = cookies.split("=")[1];
+    try {
+      const response = await axios({
+        method: methodType,
+        url: url,
+        data: data,
+        withCredentials: true,
+        headers: { Authorization: token },
+      });
+      console.log(response.data);
+      
+      return response;
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
   };
+  const clickEmp = async (co_CD) => {
+    const empData = ''
+    try {
+      const empData = await asyncRequest("system/admin/groupManage/CompanyDetail/" + co_CD, 'get');
+      formDataSet(empData.data);
+    } catch (error) {
+      console.error(error);
+    }
+    return empData;
+  };
+
+  const clickAddBtn= () => {
+    console.log("안녕".formData);
+    formDataSet(prevFormData => ({ 
+      ...prevFormData, 
+      acct_FG:"" ,
+      business:"" ,
+      ceo_NM: "",
+      ceo_TEL:"" ,
+      close_DT: "",
+      co_CD: "",
+      co_FG: "",
+      co_NB: "",
+      co_NM: "",
+      co_NMK: "",
+      est_DT: "",
+      ho_ADDR: "",
+      ho_ADDR1: "",
+      ho_FAX: "",
+      ho_ZIP: "",
+      jongmok: "",
+      open_DT: "",
+      pic_FILE_ID: "",
+      ppl_NB: "",
+      reg_NB: "",
+      use_YN: "",
+    }));
+  }
+
+
+  React.useEffect(() => {
+   
+    const fetchData = async () => {
+      try {
+        const response = await asyncRequest("system/admin/groupManage/CompanySelect", 'get');
+        const first_formData = await asyncRequest("system/admin/groupManage/CompanyDetail/" + response.data[0].co_CD
+        , 'get');
+
+        //formSetting(response.data,first_formData.data )
+        listDataSet(response.data);
+        formDataSet(first_formData.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+
+  },[])
+
+
+
+
 
   return (
     <div style={selectListWrapper}>
@@ -17,20 +102,26 @@ const SelectListWrapper = ({ width, title, dataCount, data }) => {
         <span className="listBoxSort">정렬순</span>
       </div>
       <div className="listWrapper">
+      {listData && listData.map((data) => (
         <ListBoxItem
-          leftTop={"0020"}
-          rightTop={"안녕하세요"}
-          leftBottom={"반갑습니다"}
+          clickEmp = {clickEmp} 
+          key={data.co_CD} // 각 항목의 고유한 키를 지정해주어야 합니다.
+          leftTop={data.co_CD} // 데이터의 실제 필드 값을 사용하도록 수정
+          rightBotton={data.co_FG} // 데이터의 실제 필드 값을 사용하도록 수정
+          rightTop={data.co_NM} // 데이터의 실제 필드 값을 사용하도록 수정
+          leftBottom={data.ceo_NM} // 데이터의 실제 필드 값을 사용하도록 수정
         />
-        {/* {data.map((data) => (
-          <ListBoxItem data={data} />
-        ))} */}
-      </div>
-      <div className="footerBox">
+      ))}
+    </div>
+      <div className="footerBox" onClick={clickAddBtn}>
         <i class="fa-solid fa-circle-plus"></i>추가
       </div>
     </div>
+    
   );
 };
 
+
 export default SelectListWrapper;
+
+
