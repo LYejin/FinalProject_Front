0277c723f3f8e2cdb23f84ae3c1391759b6d1db8
LYejin <vgg789@naver.com>
2023-08-18 10:50:08 +0900

    Feat : EmployeePage 구현

--- src/pages/amaranth10/EmployeePage.js
+++ src/pages/amaranth10/EmployeePage.js
@@ -23,17 +23,58 @@ import {
 import { useEffect, useState } from "react";
 import EmpSelectListWrapper from "./../../components/feature/amaranth/employee/EmpSelectListWrapper";
 import { EmpInfoBox } from "../../components/feature/amaranth/Index";
-import EmpInsertBox from "../../components/feature/amaranth/employee/EmpInsertBox";
+import { useForm } from "react-hook-form";
+import SubmitButton from "../../components/common/button/SubmitButton";
+import { getJoinTime, getNowJoinTime } from "./../../util/time";
 
 const EmployeePage = () => {
+  const { register, handleSubmit, reset, setValue } = useForm();
   const [empList, setEmpList] = useState([]);
   const [detailEmpInfo, setDetailEmpInfo] = useState();
   const [clickYN, setClickYN] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [insertButtonClick, setInsertButtonClick] = useState(false);
+  const [openDate, setOpenDate] = useState(""); // 개업일 선택 상태 관리
+  const [onChangeForm, setChangeForm] = useState(false); // 폼 변경 사항 확인
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+    kor_NM: "",
+    email_ADD: "",
+    gender_FG: "",
+    emp_CD: "",
+    enrl_FG: "",
+    join_DT: "",
+    personal_MAIL: "",
+    personal_MAIL_CP: "",
+    salary_MAIL: "",
+    tel: "",
+    home_TEL: "",
+    zipcode: "",
+    addr: "",
+    addr_NUM: "",
+  });
 
-  // const { data, error, isLoading } = useGetEmpList();
-  // console.log(data);
+  const resetData = () => {
+    setData({
+      username: "",
+      password: "",
+      kor_NM: "",
+      email_ADD: "",
+      gender_FG: "",
+      emp_CD: "",
+      enrl_FG: "",
+      join_DT: "",
+      personal_MAIL: "",
+      personal_MAIL_CP: "",
+      salary_MAIL: "",
+      tel: "",
+      home_TEL: "",
+      zipcode: "",
+      addr: "",
+      addr_NUM: "",
+    });
+  };
 
   const getEmpList = async () => {
     try {
@@ -41,6 +82,8 @@ const EmployeePage = () => {
         "system/user/groupManage/employee/getList",
         {}
       );
+      console.log(response.data[0]);
+      setData(response.data[0]);
       setEmpList(response.data);
     } catch (error) {
       console.error("Error fetching employee list:", error);
@@ -54,6 +97,11 @@ const EmployeePage = () => {
   // click 시 사원 정보 가져오기 이벤트
   const onClickDetailEmpInfo = async (kor_NM, username) => {
     try {
+      reset();
+      if (onChangeForm === true) {
+        alert("작성중인 내용이 있습니다. 취소하시겠습니까?");
+      }
+      setChangeForm(false);
       setIsLoading(true);
       setClickYN(true);
       console.log(kor_NM, username);
@@ -61,29 +109,98 @@ const EmployeePage = () => {
         "system/user/groupManage/employee/empDetail",
         { kor_NM: kor_NM, username: username }
       );
-      console.log("hiii");
-      console.log(response.data);
-      setDetailEmpInfo(response.data);
+      setData(response.data);
       setIsLoading(false);
     } catch (error) {
       console.error("Error fetching employee list:", error);
     }
   };
 
+  const onChangeFunction = (e) => {
+    console.log(e.target.value);
+    setChangeForm(true);
+    console.log(data);
+  };
+
   // 사원 insert 이벤트
-  const onClickInsertEmp = async (data) => {
+  const onClickInsertEmpBox = () => {
+    reset();
+    resetData();
+    setClickYN(false);
+  };
+
+  // 사원 remove 이벤트
+  const onClickButtonRemoveEmp = async (kor_NM, username) => {
     try {
-      setIsLoading(true);
-      setInsertButtonClick(true);
-      setClickYN(false);
+      console.log("clickemovebutton");
       const response = await authAxiosInstance.post(
-        "system/user/groupManage/employee/empInsert",
-        { data }
+        "system/user/groupManage/employee/empRemove",
+        { kor_NM, username }
       );
       console.log("hiii");
       console.log(response.data);
-      setDetailEmpInfo(response.data);
-      setIsLoading(false);
+      setClickYN(false);
+    } catch (error) {
+      console.error("Error fetching employee list:", error);
+    }
+  };
+
+  // 사원 insert 이벤트
+  const onSubmit = async (data) => {
+    try {
+      console.log("clickEmpBoxYN", clickYN);
+      console.log("insertButtonClick", insertButtonClick);
+      // 사원 update 중일 때 저장버튼 기능
+      if (clickYN && !insertButtonClick) {
+        console.log("update 버튼");
+        console.log(data);
+        const response = await authAxiosInstance.post(
+          "system/user/groupManage/employee/empUpdate",
+          { data }
+        );
+        console.log("hiii");
+        console.log(response.data);
+      }
+      // 사원 insert 중일 때 저장버튼 기능
+      if (!clickYN && insertButtonClick) {
+        console.log("insert 버튼");
+        console.log(openDate);
+        const getJoinDT = getNowJoinTime(openDate);
+        console.log(getJoinDT);
+        const response = await authAxiosInstance.post(
+          "system/user/groupManage/employee/empInsert",
+          {
+            username: data.username || "",
+            password: data.password || "",
+            kor_NM: data.kor_NM || "",
+            email_ADD: data.email_ADD || "",
+            gender_FG: "W",
+            emp_CD: data.emp_CD || "",
+            enrl_FG: "0",
+            join_DT: getJoinDT || "",
+            personal_MAIL: data.personal_MAIL || "",
+            personal_MAIL_CP: data.personal_MAIL_CP || "",
+            salary_MAIL: data.salary_MAIL || "",
+            tel: data.tel || "",
+            home_TEL: data.home_TEL || "",
+            zipcode: data.zipcode || "",
+            addr: data.addr || "",
+            addr_NUM: data.addr_NUM || "",
+          }
+        );
+        console.log(response.data);
+        console.log(getJoinDT);
+        setEmpList([
+          ...empList,
+          {
+            join_DT: getJoinDT,
+            username: data.username,
+            kor_NM: data.kor_NM,
+          },
+        ]);
+        alert("사원이 추가되었습니다.");
+        reset();
+      }
     } catch (error) {
       console.error("Error fetching employee list:", error);
     }
@@ -99,7 +216,7 @@ const EmployeePage = () => {
         <Title titleName={"상용직관리"}></Title>
         <DetailContentWrapper>
           <SelectBoxWrapper>
-            <span className="rightPadding">회사</span>
+            <span className="rightSelectBoxPadding">회사</span>
             <SelectBox width={200} />
             <span className="leftSelectBoxPadding">재직구분</span>
             <CheckSelectBox width={"200px"} />
@@ -107,7 +224,7 @@ const EmployeePage = () => {
             <TextFieldBox width={"200px"} />
             <div className="selectBoxButtonWrapper">
               <Button
-                data={<i class="fa-solid fa-magnifying-glass"></i>}
+                data={<i className="fa-solid fa-magnifying-glass"></i>}
                 width={"-10px"}
                 height={30}
               />
@@ -120,22 +237,40 @@ const EmployeePage = () => {
               dataCount={527}
               data={empList}
               clickBoxEvent={onClickDetailEmpInfo}
-              clickInsertEvent={onClickInsertEmp}
+              clickInsertBoxEvent={onClickInsertEmpBox}
             />
             <RightContentWrapper>
               <DetailTitle detailTitle={"상세정보"}></DetailTitle>
-              <div className="tableHeader">
-                기본정보 <Button data={"저장"} width={"-10px"} height={30} />
-                <Button data={"삭제"} width={"-10px"} height={30} />
-              </div>
-              <ScrollWrapper width={"100%"} height={"calc(100% - 40px)"}>
-                {/* 테이블 입력 */}
-                {clickYN && !isLoading && (
-                  <EmpInfoBox detailEmpInfo={detailEmpInfo} />
-                )}
-                {/* 사원 insert 시에 생성 */}
-                {insertButtonClick && !isLoading && <EmpInsertBox />}
-              </ScrollWrapper>
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                onChange={onChangeFunction}
+              >
+                <div className="tableHeader">
+                  기본정보{" "}
+                  <SubmitButton data={"저장"} width={"-10px"} height={30} />
+                  <Button data={"삭제"} width={"-10px"} height={30} />
+                </div>
+                <ScrollWrapper width={"100%"} height={"600px"}>
+                  {/* 테이블 입력 */}
+                  {/* 
+                  {clickYN && !isLoading && (
+                    <EmpInfoBox detailEmpInfo={detailEmpInfo} />
+                  )}
+                  {insertButtonClick && !isLoading && (
+                    <EmpInsertBox2
+                      register={register}
+                      setOpenDate={setOpenDate}
+                      openDate={openDate}
+                    />
+                  )} */}
+                  <EmpInfoBox
+                    data={data}
+                    register={register}
+                    setOpenDate={setOpenDate}
+                    openDate={openDate}
+                  />
+                </ScrollWrapper>
+              </form>
             </RightContentWrapper>
           </MainContentWrapper>
         </DetailContentWrapper>
