3ac570d0066390b5baaa21ef3ab874c23ec02d4f
LYejin <vgg789@naver.com>
2023-08-22 17:04:57 +0900

    Fix : cookies 수정

--- src/cookie/Cookie.js
+++ src/cookie/Cookie.js
@@ -2,12 +2,8 @@ import { Cookies } from 'react-cookie';
 
 const cookies = new Cookies();
 
-export const setAccessToken = (accessToken) => {
-  const today = new Date();
-  const expireDate = today.setDate(today.getDate() + 5);
-
+export const setAccessToken = accessToken => {
   return cookies.set('accessToken', accessToken, {
-    expires: new Date(expireDate),
     httpOnly: false,
     sameSite: 'strict', // CSRF를 방지
     path: '/',
