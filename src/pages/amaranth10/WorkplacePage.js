228fd7d2fac6721de98cec5e2c68e48b255db41b
NamDongWoo <wowdong27@naver.com>
2023-08-22 16:23:21 +0900

    Feat : 사업장 수정 기능 추가 / 알림창 추가

--- src/pages/amaranth10/WorkplacePage.js
+++ src/pages/amaranth10/WorkplacePage.js
@@ -27,11 +27,16 @@ import axios from '../../../node_modules/axios/index';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { getAccessToken } from '../../cookie/Cookie';
+import Swal from 'sweetalert2';
 
 const WorkplacePage = () => {
+  const [companyData, setCompanyData] = useState([]);
   const [workplaceData, setWorkplaceData] = useState([]);
   const [workplaceDetailData, setWorkplaceDetailData] = useState([]);
+  const [isAdding, setIsAdding] = useState(false);
+
   useEffect(() => {
+    fetchCompanyData();
     fetchWorkplaceData();
   }, []);
 
@@ -66,40 +71,71 @@ const WorkplacePage = () => {
     }
   };
 
+  // const FetchWorkplaceDetailInfo = async divCd => {
+  //   try {
+  //     const response = await axios.get(
+  //       `system/user/WorkplaceManage/getWorkpInfo/${divCd}`,
+  //       { headers: { Authorization: getAccessToken() } }
+  //     );
+  //     setWorkplaceDetailData(response.data);
+  //   } catch (error) {
+  //     console.error('Error fetching workplace info:', error);
+  //   }
+  // };
+
   const FetchWorkplaceDetailInfo = async divCd => {
     try {
       const response = await axios.get(
         `system/user/WorkplaceManage/getWorkpInfo/${divCd}`,
-
         { headers: { Authorization: getAccessToken() } }
       );
-      // console.log('가져온 사업장 정보:', response.data);
-      // console.log('전번 : ', response.data.div_TEL);
-      setWorkplaceDetailData(response.data);
+
+      const fetchedWorkplaceDetailData = response.data;
+
+      try {
+        const companyResponse = await axios.get(
+          `system/admin/groupManage/CompanyDetail/${fetchedWorkplaceDetailData.co_CD}`,
+          { headers: { Authorization: getAccessToken() } }
+        );
+
+        const companyData = companyResponse.data;
+        const updatedWorkplaceDetailData = {
+          ...fetchedWorkplaceDetailData,
+          co_NM: companyData.co_NM,
+        };
+
+        setWorkplaceDetailData(updatedWorkplaceDetailData);
+      } catch (error) {
+        console.error('Error fetching company detail:', error);
+      }
     } catch (error) {
       console.error('Error fetching workplace info:', error);
     }
   };
 
-  const handleInsert = async () => {
-    // WorkPlaceInfoWrapper 컴포넌트 내의 input 데이터 수집
-    console.log('insert함수 실행!');
-    console.log('맛증ㄹ까?', inputRefs.divNMRef);
+  const handleAddClick = () => {
+    setIsAdding(true);
+  };
+
+  const createFormData = inputRefs => {
     const formData = new FormData();
-    formData.append('DIV_CD', '015'); // div_CD ref 활용
-    formData.append('CO_CD', '11');
-    formData.append('div_NM', inputRefs.divNMRef.current.value); // div_NM ref 활용
-    formData.append('div_ADDR', inputRefs.divADDRRef.current.value); // div_ADDR ref 활용
-    formData.append('div_TEL', inputRefs.divTELRef.current.value); // div_TEL ref 활용
-    formData.append('reg_NB', inputRefs.regNBRef.current.value); // reg_NB ref 활용
-    formData.append('div_TO_CD', '121'); // div_TO_CD ref 활용
-    formData.append('div_NMK', inputRefs.divNMKRef.current.value); // div_NMK ref 활용
-    formData.append('business', inputRefs.businessRef.current.value); // business ref 활용
-    formData.append('jongmok', inputRefs.jongmokRef.current.value); // jongmok ref 활용
-    formData.append('mas_NM', inputRefs.masNMRef.current.value);
-    console.log('divNMRef.current:', inputRefs.divNMRef.current.value);
-    const divNMValue = formData.get('div_CD');
-    console.log('div_CD:', divNMValue);
+    formData.append('DIV_CD', inputRefs.divCDRef?.current?.value || '');
+    formData.append('CO_CD', '11'); // 회사코드 업데이트 필요 시 추가
+    formData.append('DIV_NM', inputRefs.divNMRef?.current?.value || '');
+    formData.append('DIV_ADDR', inputRefs.divADDRRef?.current?.value || '');
+    formData.append('DIV_TEL', inputRefs.divTELRef?.current?.value || '');
+    formData.append('REG_NB', inputRefs.regNBRef?.current?.value || '');
+    formData.append('DIV_TO_CD', '121'); // 업태코드 업데이트 필요 시 추가
+    formData.append('DIV_NMK', inputRefs.divNMKRef?.current?.value || '');
+    formData.append('BUSINESS', inputRefs.businessRef?.current?.value || '');
+    formData.append('JONGMOK', inputRefs.jongmokRef?.current?.value || '');
+    formData.append('MAS_NM', inputRefs.masNMRef?.current?.value || '');
+    return formData;
+  };
+
+  const handleInsert = async () => {
+    const formData = createFormData(inputRefs, workplaceDetailData);
+
     for (const [key, value] of formData.entries()) {
       console.log(key, value);
     }
@@ -109,10 +145,75 @@ const WorkplacePage = () => {
         formData,
         { headers: { Authorization: getAccessToken() } }
       );
+
       console.log('Insert response:', response.data);
-      // 성공적으로 요청을 처리한 후에 추가 작업 수행 가능
+
+      Swal.fire({
+        icon: 'success',
+        title: '저장 완료',
+        text: '작업장 정보가 성공적으로 저장되었습니다.',
+      });
+      fetchWorkplaceData();
     } catch (error) {
       console.error('Error inserting workplace:', error);
+      Swal.fire({
+        icon: 'error',
+        title: '저장 실패',
+        text: '작업장 정보 저장에 실패했습니다. 다시 시도해주세요.',
+      });
+    }
+  };
+
+  const handleUpdate = async () => {
+    console.log('update 함수 실행!');
+    const formData = createFormData(inputRefs, workplaceDetailData);
+
+    try {
+      const response = await axios.put(
+        '/system/user/WorkplaceManage/update',
+        formData,
+        { headers: { Authorization: getAccessToken() } }
+      );
+
+      if (response.status === 200) {
+        Swal.fire({
+          icon: 'success',
+          title: '업데이트 완료',
+          text: '작업장 정보가 성공적으로 업데이트되었습니다.',
+        });
+        fetchWorkplaceData();
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: '업데이트 실패',
+          text: '작업장 정보 업데이트에 실패했습니다. 다시 시도해주세요.',
+        });
+      }
+    } catch (error) {
+      console.error('Error updating workplace:', error);
+      Swal.fire({
+        icon: 'error',
+        title: '업데이트 실패',
+        text: '작업장 정보 업데이트에 실패했습니다. 다시 시도해주세요.',
+      });
+    }
+  };
+
+  const fetchCompanyData = async () => {
+    try {
+      const response = await axios.get(
+        'system/admin/groupManage/CompanySelect',
+        { headers: { Authorization: getAccessToken() } }
+      );
+      const mappedCompanyData = response.data.map(company => ({
+        value: company.co_CD,
+        label: company.co_NM,
+      }));
+
+      setCompanyData(mappedCompanyData);
+      console.log('znlznnlznlznlz', mappedCompanyData);
+    } catch (error) {
+      console.error('Error fetching company data:', error);
     }
   };
 
@@ -143,6 +244,9 @@ const WorkplacePage = () => {
               dataCount={workplaceData.length}
               data={workplaceData}
               FetchWorkplaceDetailInfo={FetchWorkplaceDetailInfo}
+              handleAddClick={handleAddClick}
+              isAdding={isAdding}
+              setIsAdding={setIsAdding}
             />
             <RightContentWrapper>
               <WorkpHeadTitle
@@ -152,8 +256,10 @@ const WorkplacePage = () => {
               <ScrollWrapper width={'100%'} height={'100%'}>
                 {workplaceDetailData && (
                   <WorkPlaceInfoWrapper
-                    data={workplaceDetailData}
+                    data={isAdding ? '' : workplaceDetailData}
                     inputRefs={inputRefs}
+                    isAdding={isAdding}
+                    companyData={companyData}
                   />
                 )}
               </ScrollWrapper>
