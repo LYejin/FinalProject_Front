b3a2ee7e7e3846f1dda57ad98e200b5a0fd3aec9
LYejin <vgg789@naver.com>
2023-08-22 17:01:44 +0900

    Feat : EmpSelectBox 구현

---
+++ src/components/feature/amaranth/employee/EmpSelectBox.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+const EmployeeSelectBox = ({ data, width }) => {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = event => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: width }} size="small">
+      <Select
+        id="demo-select-small"
+        value={age}
+        onChange={handleChange}
+        displayEmpty
+        sx={{
+          height: '28px',
+          fontSize: '0.8rem',
+        }}
+      >
+        <MenuItem value="">
+          <em>전체</em>
+        </MenuItem>
+        {data.map(company => (
+          <MenuItem value={company.co_CD}>
+            {company.co_CD} {company.co_NM}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default EmployeeSelectBox;
