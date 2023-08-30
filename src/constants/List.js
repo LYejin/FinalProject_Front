8431885f9a7853fa18e4f19189a8f93514ad9671
LYejin <vgg789@naver.com>
2023-08-30 11:51:24 +0900

    Feat : Email List 이메일 정보

--- src/constants/List.js
+++ src/constants/List.js
@@ -1,1 +1,13 @@
 export const ENRL_FG_LIST = ['재직', '휴직', '퇴직'];
+
+export const EMAIL_LIST = [
+  'naver.com',
+  'hanmail.net',
+  'gmail.com',
+  'nate.com',
+  'hotmail.com',
+  'lycos.co.kr',
+  'empal.com',
+  'dreamwiz.com',
+  'korea.com',
+];
