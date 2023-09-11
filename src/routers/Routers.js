d81a84143d3a82bb7c5d1bc476f260229da47917
LYejin <vgg789@naver.com>
2023-09-11 15:39:55 +0900

    Fix : Router Todo 삭제

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Todo from '../pages/Todo/Todo';
 import RegisterForm from '../pages/login/RegisterForm';
 import LoginHookForm from '../pages/login/LoginHookForm';
 import { URL } from '../constants/router';
@@ -18,7 +17,6 @@ const Routers = () => {
   return (
     <Routes>
       <Route path={URL.home} element={<LoginHookForm />} />
-      <Route path={URL.main} element={<Todo />} />
       <Route path={URL.register} element={<RegisterForm />} />
       <Route path={URL.company} element={<CompanyPage />} />
       <Route path={URL.employee} element={<EmployeePage />} />
