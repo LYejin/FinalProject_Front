f2e0e6ebf15b47244daef5cb47dfe09e8ba82ca1
LYejin <vgg789@naver.com>
2023-09-27 15:45:19 +0900

    Fix : SelectBox 기능 수정

--- src/components/common/box/SelectBox.js
+++ src/components/common/box/SelectBox.js
@@ -1,33 +1,48 @@
-import React from "react";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
+import React from 'react';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
-const SelectBox = ({ data, width }) => {
-  const [age, setAge] = React.useState("");
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
+const SelectBox = ({ data, width, state, setState }) => {
+  const handleChange = event => {
+    setState(event.target.value);
   };
 
   return (
-    <FormControl sx={{ m: 1, minWidth: width }} size="small">
+    <FormControl sx={{ m: 1, width: width }} size="small">
       <Select
         id="demo-select-small"
-        value={age}
+        value={state}
         onChange={handleChange}
         displayEmpty
         sx={{
-          height: "28px",
-          fontSize: "0.8rem",
+          height: '28px',
+          fontSize: '0.8rem',
+          borderRadius: '0',
+          marginLeft: '-9px',
+        }}
+        MenuProps={{
+          PaperProps: {
+            style: {
+              width: width,
+              maxHeight: 120, // 원하는 최대 높이 값으로 변경
+            },
+          },
         }}
       >
-        <MenuItem value="">
-          <em>전체</em>
-        </MenuItem>
-        <MenuItem value={10}>더존비즈온</MenuItem>
-        <MenuItem value={20}>더존비즈온2</MenuItem>
-        <MenuItem value={30}>더존비즈온3</MenuItem>
+        {data.map((value, index) => (
+          <MenuItem
+            value={index}
+            key={index}
+            style={{
+              fontSize: '0.8rem',
+              fontWeight: 'bold',
+              borderBottom: '1px solid #CCC',
+            }}
+          >
+            {value}
+          </MenuItem>
+        ))}
       </Select>
     </FormControl>
   );
